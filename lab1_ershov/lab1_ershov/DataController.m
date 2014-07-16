f057c517808dc5fa90e3a748a895bdbdac8b357e
Alexey Ershov <alaershov@gmail.com>
2014-07-16 19:07:06 +0700

    Add Singleton data storage
    Added Singleton class, DataController, that contains all Departments.

---
+++ lab1_ershov/lab1_ershov/DataController.m
@@ -0,0 +1,47 @@
+//
+// Created by Admin on 16/07/14.
+// Copyright (c) 2014 Noveo Summer Internship. All rights reserved.
+//
+
+#import "DataController.h"
+#import "Department.h"
+
+@interface DataController ()
+@property NSMutableArray *mDepartments;
+@end
+
+@implementation DataController
+
+static DataController *instance = nil;
+
++ (DataController *)sharedInstance {
+    @synchronized(self)
+    {
+        if (instance == nil)
+        {
+            instance = [[DataController alloc] init];
+        }
+    }
+
+    return instance;
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        self.mDepartments = [[NSMutableArray alloc] init];
+    }
+
+    return self;
+}
+
+
+- (void)addDepartment:(Department *)department {
+    [self.mDepartments addObject:department];
+}
+
+- (NSArray *)departments {
+    return self.mDepartments;
+}
+
+@end
