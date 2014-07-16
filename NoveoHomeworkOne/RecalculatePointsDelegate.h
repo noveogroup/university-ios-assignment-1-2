83075f38e664fcb550c7fa4b946bae361ccddbd0
Smirnov Vadim <badimm.235@gmail.com>
2014-07-16 18:39:29 +0700

    Removing some user-dependent files from git.
    Add code for Singleton and calculating average points.

---
+++ NoveoHomeworkOne/RecalculatePointsDelegate.h
@@ -0,0 +1,13 @@
+//
+//  RecalculatePointsDelegate.h
+//  NoveoHomeworkOne
+//
+//  Created by Wadim on 7/16/14.
+//  Copyright (c) 2014 Smirnov Electronics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol RecalculatePointsDelegate <NSObject>
+
+@end
