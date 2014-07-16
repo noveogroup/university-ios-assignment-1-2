7c4783752aa5d0d623625b0441635681323fdef2
Alexey Ershov <alaershov@gmail.com>
2014-07-16 19:02:03 +0700

    Add Observer pattern protocols
    Add Observer and Observable protocols.

---
+++ lab1_ershov/lab1_ershov/Observer.h
@@ -0,0 +1,10 @@
+//
+// Created by Admin on 16/07/14.
+// Copyright (c) 2014 Noveo Summer Internship. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Observer <NSObject>
+- (void)dataChanged;
+@end
