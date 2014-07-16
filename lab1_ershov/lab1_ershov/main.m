468e6c1633b773ed18e9738c33db5b0db3aa4061
Alexey Ershov <alaershov@gmail.com>
2014-07-16 19:20:09 +0700

    Create sample hierarchy
    Created sample hierarchy of objects with average marks, to test marsk counting.

--- lab1_ershov/lab1_ershov/main.m
+++ lab1_ershov/lab1_ershov/main.m
@@ -8,57 +8,90 @@
 
 #import "Student.h"
 #import "Teacher.h"
+#import "Group.h"
+#import "Department.h"
+#import "DataController.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        NSMutableArray * students = [[NSMutableArray alloc] init];
+        NSArray *students = [NSArray arrayWithObjects:
+                [[Student alloc] initWithName:@"Alex"
+                               BirthdayString:@"07/01/1994"],
+                [[Student alloc] initWithName:@"Bob"
+                               BirthdayString:@"02/02/1992"],
+                [[Student alloc] initWithName:@"Sigismund"
+                               BirthdayString:@"04/14/1992"],
+                [[Student alloc] initWithName:@"Emma"
+                               BirthdayString:@"03/23/1993"],
+                [[Student alloc] initWithName:@"John"
+                               BirthdayString:@"07/17/1994"],
+                [[Student alloc] initWithName:@"Maria"
+                               BirthdayString:@"06/06/1991"],
+                [[Student alloc] initWithName:@"Julia"
+                               BirthdayString:@"05/15/1994"],
+                [[Student alloc] initWithName:@"Abraham"
+                               BirthdayString:@"08/23/1994"],
+                [[Student alloc] initWithName:@"Geralt"
+                               BirthdayString:@"09/19/1990"],
+                [[Student alloc] initWithName:@"Triss"
+                               BirthdayString:@"10/20/1989"],
+                [[Student alloc] initWithName:@"Jennifer"
+                               BirthdayString:@"12/23/1993"],
+                [[Student alloc] initWithName:@"Leto"
+                               BirthdayString:@"11/21/1992"],
+                [[Student alloc] initWithName:@"Zoltan"
+                               BirthdayString:@"12/22/1992"],
+                [[Student alloc] initWithName:@"Vernon"
+                               BirthdayString:@"01/11/1991"],
+                nil
+        ];
 
-        [students addObject:[[Student alloc] initWithName:@"Alexandr"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"01/13/1992"]
-                                              AverageMark:4.7]];
-        [students addObject:[[Student alloc] initWithName:@"Bjorn"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"02/22/1991"]
-                                              AverageMark:5.0]];
-        [students addObject:[[Student alloc] initWithName:@"Sigismund"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"12/11/1993"]
-                                              AverageMark:4.5]];
-        [students addObject:[[Student alloc] initWithName:@"John"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"07/01/1994"]
-                                              AverageMark:4.6]];
-        [students addObject:[[Student alloc] initWithName:@"Jose"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"03/19/1990"]
-                                              AverageMark:4.3]];
-        [students addObject:[[Student alloc] initWithName:@"Ibragim"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"04/27/1992"]
-                                              AverageMark:3.9]];
-        [students addObject:[[Student alloc] initWithName:@"Maria"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"03/25/1992"]
-                                              AverageMark:4.7]];
-        [students addObject:[[Student alloc] initWithName:@"Harry"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"05/03/1993"]
-                                              AverageMark:3.7]];
-        [students addObject:[[Student alloc] initWithName:@"Jerry"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"07/04/1991"]
-                                              AverageMark:3.5]];
-        [students addObject:[[Student alloc] initWithName:@"Jango"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"11/10/1993"]
-                                              AverageMark:4.9]];
-        [students addObject:[[Student alloc] initWithName:@"Emma"
-                                                 Birthday:[NSDate dateWithNaturalLanguageString:@"10/09/1993"]
-                                              AverageMark:4.1]];
+        // Groups
+        NSArray *groups = [NSArray arrayWithObjects:
+                [[Group alloc] initWithName:@"1201"],
+                [[Group alloc] initWithName:@"1202"],
+                [[Group alloc] initWithName:@"2201"],
+                [[Group alloc] initWithName:@"2202"],
+                nil
+        ];
 
-        Teacher *teacher = [[Teacher alloc] init];
-        teacher.name = @"Minerva MacGonagall";
-        teacher.birthday = [NSDate dateWithNaturalLanguageString:@"10/09/1953"];
-        teacher.salary = 25500;
+        // Departments
+        NSArray *departments = [NSArray arrayWithObjects:
+                [[Department alloc] initWithName:@"Information Technology"],
+                [[Department alloc] initWithName:@"Wizardry and Witchcraft"],
+                nil
+        ];
 
-        for (int i = 0; i < [students count] / 2; ++i) {
-            [teacher addStudent:[students objectAtIndex:(NSUInteger) i]];
+        // add groups to departments
+        Department *tempDepartment = [departments objectAtIndex:0];
+        [tempDepartment addGroup:[groups objectAtIndex:0]];
+        [tempDepartment addGroup:[groups objectAtIndex:1]];
+
+        tempDepartment = [departments objectAtIndex:1];
+        [tempDepartment addGroup:[groups objectAtIndex:2]];
+        [tempDepartment addGroup:[groups objectAtIndex:3]];
+
+        // add random marks to all students, and
+        // add students to groups
+        for (Student *student in students) {
+            for (int i = 0; i < 7; ++i) {
+                int randomMark = arc4random_uniform(6);
+                [student addMark:randomMark];
+            }
+            [[groups objectAtIndex:[students indexOfObject:student] % 4] addStudent:student];
         }
 
-        NSLog([teacher description]);
+        DataController *dataController = [DataController sharedInstance];
+
+        for (Department *department in departments) {
+            [dataController addDepartment:department];
+        }
+
+        for (Department *department in dataController.departments) {
+            NSLog([department description]);
+        }
 
     }
     return 0;
