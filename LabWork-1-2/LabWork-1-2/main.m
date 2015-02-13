f23a7aca2d023c36c460af36a431e9bc9c082d48
Konstantin Mamaev <kostya.plhs@gmail.com>
2015-02-13 21:00:52 +0600

    Add new project

---
+++ LabWork-1-2/LabWork-1-2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LabWork-1-2
+//
+//  Created by Konstantin on 13/02/15.
+//  Copyright (c) 2015 Konstantin inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
