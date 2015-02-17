fe44a3323f46f3bd8d1f22c127ffbab601e5fe0b
Konstantin Mamaev <kostya.plhs@gmail.com>
2015-02-17 14:37:20 +0600

    Simplify notations to initialize arrays and to access toit's elements

--- LabWork-1-2/LabWork-1-2/main.m
+++ LabWork-1-2/LabWork-1-2/main.m
@@ -9,7 +9,7 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // Initialize students
-        NSArray *students = [NSArray arrayWithObjects:
+        NSArray *students = @[
                              [[Student alloc] initWithName:@"Ivan" age:@16 gradePointAverage:@4.3],
                              [[Student alloc] initWithName:@"Petr" age:@17 gradePointAverage:@3.4],
                              [[Student alloc] initWithName:@"Ibragim" age:@18 gradePointAverage:@4.5],
@@ -19,31 +19,31 @@ int main(int argc, const char * argv[]) {
                              [[Student alloc] initWithName:@"Elena" age:@17 gradePointAverage:@4.1],
                              [[Student alloc] initWithName:@"Olesya" age:@18 gradePointAverage:@4.7],
                              [[Student alloc] initWithName:@"Olya" age:@17 gradePointAverage:@3.9],
-                             [[Student alloc] initWithName:@"Snezhana" age:@17 gradePointAverage:@4.6],
-                             nil];
+                             [[Student alloc] initWithName:@"Snezhana" age:@17 gradePointAverage:@4.6]
+        ];
         
         // Initialize teacher
-        NSArray *teachers = [NSArray arrayWithObjects:
+        NSArray *teachers = @[
                              [[Teacher alloc] initWithName:@"Igor Andreevich" age:@57 salary:@50000],
                              [[Teacher alloc] initWithName:@"Petr Petrovich" age:@36 salary:@40000],
-                             [[Teacher alloc] initWithName:@"Zurab Konstantinovich" age:@48 salary:@45000],
-                             nil];
+                             [[Teacher alloc] initWithName:@"Zurab Konstantinovich" age:@48 salary:@45000]
+        ];
         
         // Initialize head of the chair
         HeadOfTheChair *head = [[HeadOfTheChair alloc] initWithName:@"Vladimir Vladimirovich" age:@55 salary:@70000];
         
         // Initialize some relations between participants of educational process
-        [head addInferior:[teachers objectAtIndex:0]];
-        [[teachers objectAtIndex:1] addSuperior:head];
-        [[teachers objectAtIndex:0] addInferior:[students objectAtIndex:0]];
-        [[teachers objectAtIndex:0] addInferior:[students objectAtIndex:1]];
-        [[teachers objectAtIndex:0] addInferior:[students objectAtIndex:2]];
-        [[teachers objectAtIndex:1] addInferior:[students objectAtIndex:1]];
-        [[teachers objectAtIndex:1] addInferior:[students objectAtIndex:2]];
-        [[teachers objectAtIndex:1] addInferior:[students objectAtIndex:3]];
-        [[students objectAtIndex:4] addSuperior:[teachers objectAtIndex:0]];
-        [[students objectAtIndex:5] addSuperior:[teachers objectAtIndex:1]];
-        [[students objectAtIndex:6] addSuperior:[teachers objectAtIndex:1]];
+        [head addInferior:teachers[0]];
+        [teachers[1] addSuperior:head];
+        [teachers[0] addInferior:students[0]];
+        [teachers[0] addInferior:students[1]];
+        [teachers[0] addInferior:students[2]];
+        [teachers[1] addInferior:students[1]];
+        [teachers[1] addInferior:students[2]];
+        [teachers[1] addInferior:students[3]];
+        [students[4] addSuperior:teachers[0]];
+        [students[5] addSuperior:teachers[1]];
+        [students[6] addSuperior:teachers[1]];
         
         // Output the result
         NSLog(@"All the created entities:\n%@\n%@\n%@", students, teachers, head);
@@ -51,21 +51,21 @@ int main(int argc, const char * argv[]) {
         // Initialize department of university and add some entities to it
         DepartmentOfUniversity *department = [[DepartmentOfUniversity alloc] init];
         [[[[[[[[[[department assignHeadOfTheChair:head]
-        addTeacher:[teachers objectAtIndex:0]]
-        addTeacher:[teachers objectAtIndex:1]]
-        addStudent:[students objectAtIndex:0]]
-        addStudent:[students objectAtIndex:1]]
-        addStudent:[students objectAtIndex:2]]
-        addStudent:[students objectAtIndex:3]]
-        addStudent:[students objectAtIndex:4]]
-        addStudent:[students objectAtIndex:5]]
-        addStudent:[students objectAtIndex:6]];
+        addTeacher:teachers[0]]
+        addTeacher:teachers[1]]
+        addStudent:students[0]]
+        addStudent:students[1]]
+        addStudent:students[2]]
+        addStudent:students[3]]
+        addStudent:students[4]]
+        addStudent:students[5]]
+        addStudent:students[6]];
         
         // Output avarage GPA of the department
         NSLog(@"Avarage GPA of the department: %0.2f", department.avarageGPA);
         
         // Change GPA of one of the department's students and output updated value of avarage GPA of the department
-        [[students objectAtIndex:0] setGradePointAverage:@5.0];
+        [students[0] setGradePointAverage:@5.0];
         NSLog(@"Updated avarage GPA of the department: %0.2f", department.avarageGPA);
         
         // Initialize the university and output all it's participants
